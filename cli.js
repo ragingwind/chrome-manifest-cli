e7335c74c8f27cdb9992a0f717fd32f8daaefc7a
ragingwind <ragingwind@gmail.com>
2016-04-26 10:10:10 +0900

    Using chrome-manifest

--- cli.js
+++ cli.js
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 'use strict';
-var meow = require('meow');
-var chromeManifestCli = require('./');
+const meow = require('meow');
+const cm = require('chrome-manifest');
+const decamelizeKeys = require('decamelize-keys');
 
-var cli = meow([
-	'Usage',
-	'  $ chrome-manifest-cli [input]',
-	'',
-	'Options',
-	'  --foo  Lorem ipsum. [Default: false]',
-	'',
-	'Examples',
-	'  $ chrome-manifest-cli',
-	'  unicorns & rainbows',
-	'  $ chrome-manifest-cli ponies',
-	'  ponies & rainbows'
-]);
+const cli = meow([`
+  Usage
+    $ chrome-manifest command target <options>
+
+	Commands
+		create: create manifest.json by following target types, app or [ext|extension]
+		add: add following properties to exist manifest.json with template
+		remove: remove following properties from exist manifest.json
+
+  Options
+		--manifest: path of manifest.json to write or read
+    --channel: [stable|dev|beta|trunk]
+
+  Examples
+    $ chrome-manifest-cl,
+    unicorns & rainbow,
+    $ chrome-manifest-cli ponie,
+    ponies & rainbow
+`]);
+
+const flags = decamelizeKeys(cli.flags, '_');
 
 console.log(chromeManifestCli(cli.input[0] || 'unicorns'));
